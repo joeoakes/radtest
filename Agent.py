1e1c06672ffc132db3f07dd0d2baf192c18fe524
joe oakes <joe.oakes@gmail.com>
2022-10-05 14:19:10 -0400

    added Agent Class

---
+++ Agent.py
@@ -0,0 +1,11 @@
+#Agent Class
+#Properties Name, ListofWebsites, Starttime, Status, ScrapeTYpe, Database, APIUsed
+class Agent:
+    def __init__(self,name, listofWebsites, starttime, status, scrapeType, database, apiUsed):
+        self.name = name
+        self.listofWebsites = listofWebsites
+        self.starttime = starttime
+        self.status = status
+        self.scrapeTyhpe = scrapeType
+        self.database = database
+        self.apiUsed = apiUsed
