0699b83f1a7db8a6c43a42d1b60a40262a1cce8d
joeoakes <jxo19@psu.edu>
2022-10-05 21:47:13 -0400

    database enum

--- DataStore.py
+++ DataStore.py
@@ -5,32 +5,30 @@
 #make sure to perform pip install pymongo
 from pymongo import MongoClient
 
-class DataStore:
-    def connectDatabase(self, databaseType, database, collection):
-        if (databaseType == DatabaseTypes.json):
-            try:
-             configs = open('radscrap.json', 'r')
-             content = configs.read()
-             configs.close()
-             return content
-            except:
-                e = sys.exc_info()[0]
-        elif (databaseType == DatabaseTypes.mongodb):
-            try:
-             clientDB = MongoClient('localhost', 27017)
-             db = clientDB.test_database
-            except:
-                e = sys.exc_info()[0]
-        elif (databaseType == Databases.redis):
-            try:
-              None
-            except:
-                e = sys.exc_info()[0]
-        elif (databaseType == Databases.cosmos):
-            try:
-                None
-            except:
-                e = sys.exc_info()[0]
+class DataStore():
+    def __init__(self, databaseType, databaseName, databaseCollection):
+        self.databaseType = databaseType
+        self.databaseName = databaseName
+        self.databaseCollection = databaseCollection
+class Json(DataStore):
+    try:
+        configs = open('radscrap.json', 'r')
+        content = configs.read()
+        configs.close()
+    except:
+        e = sys.exc_info()[0]
+class MongoDb(DataStore):
+    try:
+        clientDB = MongoClient('localhost', 27017)
+        db = clientDB.test_database
+    except:
+        e = sys.exc_info()[0]
+class Redis(DataStore):
+    try:
+        None
+    except:
+        e = sys.exc_info()[0]
+
     def getResourceById(self, resourceId):
         self.get(resourceId)
     def setResource( resourceId, resourceContent):
