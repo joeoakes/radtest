13b147fb1a542501640eee81ae2364380079cea3
joeoakes <jxo19@psu.edu>
2022-10-05 23:31:40 -0400

    datastore class mods

--- JsonDS.py
+++ JsonDS.py
@@ -1,19 +1,22 @@
-#Purpose: JSON class for RadTest
-#Author: Joe Oakes, Hof Uni, PSA
-#Date 10/5/2022
+# Purpose: JSON class for RadTest
+# Author: Joe Oakes, Hof Uni, PSA
+# Date 10/5/2022
 
 #Logging facility for Python
 import logging
 import sys
 
-
 class JsonDS:
+
  def __init__(self, filename):
-   try:
-    configs = open(filename, 'r')
-    content = configs.read()
-    configs.close()
-   except:
-    logging.error(str(sys.exc_info()[0]))
+  self.filename = filename
+  self.content = ''
+
  def getcontent(self):
-     return self.content
+  try:
+    configs = open(self.filename, 'r')
+    self.content = configs.read()
+    configs.close()
+  except:
+    logging.error(str(sys.exc_info()[0]))
+  return self.content
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-#Purpose: Python3 Web Scraping project for Radwell.
-#Author: Joe Oakes, Hof Uni, PSA
-#Date 10/5/2022
+# Purpose: Python3 Web Scraping project for Radwell.
+# Author: Joe Oakes, Hof Uni, PSA
+# Date 10/5/2022
 
 # importing python modules
 import urllib.request
 import json
-#from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import sys
+from JsonDS import JsonDS
 
-
-#Create the json object to
+# Create the json object to
 jsonDoc = JsonDS('radscrap.json')
-content = json.getcontent()
+content = jsonDoc.getcontent()
+print(content)
 
   # opening the url for reading
   #url = urllib.request.urlopen(json.loads(content))
